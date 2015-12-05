dfb88fa6ebe4140ecca095920617553590afefeb
juanjo <juanjo@zero>
2015-12-05 19:56:07 +0100

    fixes

--- src/youtubeuncensor/ExplorerController.java
+++ src/youtubeuncensor/ExplorerController.java
@@ -1,8 +1,11 @@
 package youtubeuncensor;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -48,7 +51,7 @@ public class ExplorerController implements Initializable {
         menu_choiceKeyword.getSelectionModel().selectFirst();
 
         menu_choiceKeyword.valueProperty().addListener((observable, oldValue, newValue) -> {
-                listVideos();
+            listVideos();
         });
 
         listVideos();
@@ -68,7 +71,7 @@ public class ExplorerController implements Initializable {
         } else {
             for (TaskItem item : Main.taskList) {
                 if (item.getKeyword().equals(menu_choiceKeyword.getSelectionModel().selectedItemProperty().toString())) {
-                   
+
                     addVideosFromKeyword(item);
                     break;
                 }
@@ -89,51 +92,59 @@ public class ExplorerController implements Initializable {
 
             if (imageFile.exists()) {
 
-                ImageView image = new ImageView(imageFile.toURI().toString());
-                image.setFitWidth(200);
-                image.setFitHeight(200);
-
-                MenuItem item1 = new MenuItem("Delete");
-                item1.setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        //System.out.println(file.getName());
-                        String nvideo = file.getName().replace(".mp4", "");
-                        
-                        File thumbnail = new File(item.getDirectory()+"/"+nvideo+".jpg");
-                        File metadata = new File(item.getDirectory()+"/"+nvideo+".info.json");
-                        
-                        if (thumbnail.exists()){
-                            thumbnail.delete();
-                        }
-                        
-                        if (metadata.exists()){
-                            metadata.delete();
-                        }
-                        
-                        file.delete();
-                        
-                        listVideos();
-                    }
-                });
-
-                ContextMenu cm = new ContextMenu();
-                cm.getItems().add(item1);
-
-                image.setOnMousePressed(new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent event) {
-                        if (event.isSecondaryButtonDown()) {
-                            cm.show(image, event.getScreenX(), event.getScreenY());
-                        }
-                    }
-                });
-
-                this.flowpane.getChildren().add(image);
+                addVideo(item, file, imageFile);
+                
             } else {
-                this.flowpane.getChildren().add(new ImageView(new Image(getClass().getResourceAsStream("youtube_uncensor.png"))));
+                
+               addVideo(item, file, new File(""));
+
             }
 
         }
+    }
+
+    private void addVideo(TaskItem item, File file, File imageFile) {
+        
+        ImageView image = new ImageView(imageFile.toURI().toString());
+        image.setFitWidth(200);
+        image.setFitHeight(200);
+
+        MenuItem item1 = new MenuItem("Delete");
+        item1.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                //System.out.println(file.getName());
+                String nvideo = file.getName().replace(".mp4", "");
+
+                File thumbnail = new File(item.getDirectory() + "/" + nvideo + ".jpg");
+                File metadata = new File(item.getDirectory() + "/" + nvideo + ".info.json");
+
+                if (thumbnail.exists()) {
+                    thumbnail.delete();
+                }
+
+                if (metadata.exists()) {
+                    metadata.delete();
+                }
+
+                file.delete();
+
+                listVideos();
+            }
+        });
+
+        ContextMenu cm = new ContextMenu();
+        cm.getItems().add(item1);
+
+        image.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.isSecondaryButtonDown()) {
+                    cm.show(image, event.getScreenX(), event.getScreenY());
+                }
+            }
+        });
+
+        this.flowpane.getChildren().add(image);
     }
 
 }
