bbe8fad2e90b25fc7f1daf63b40bfc27e85fae1d
juanjo <juanjo@zero>
2015-12-05 18:51:37 +0100

    wip

--- src/youtubeuncensor/ExplorerController.java
+++ src/youtubeuncensor/ExplorerController.java
@@ -5,11 +5,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import youtubeuncensor.core.TaskItem;
 
@@ -62,21 +67,21 @@ public class ExplorerController implements Initializable {
         if (keyword == null) {
             for (TaskItem item : Main.taskList) {
 
-                addVideo(item);
+                addVideosFromKeyword(item);
 
             }
         } else {
             for (TaskItem item : Main.taskList) {
                 if (item.getKeyword().equals(keyword)) {
-                    //TODO
-                    addVideo(item);
+                   
+                    addVideosFromKeyword(item);
                     break;
                 }
             }
         }
     }
 
-    private void addVideo(TaskItem item) {
+    private void addVideosFromKeyword(TaskItem item) {
 
         //TODO BETTER
         File[] files = item.getVideoFiles();
@@ -92,6 +97,25 @@ public class ExplorerController implements Initializable {
                 ImageView image = new ImageView(imageFile.toURI().toString());
                 image.setFitWidth(200);
                 image.setFitHeight(200);
+
+                MenuItem item1 = new MenuItem("Delete");
+                item1.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e) {
+                        System.out.println("About");
+                    }
+                });
+
+                ContextMenu cm = new ContextMenu();
+                cm.getItems().add(item1);
+
+                image.setOnMousePressed(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.isSecondaryButtonDown()) {
+                            cm.show(image, event.getScreenX(), event.getScreenY());
+                        }
+                    }
+                });
 
                 this.flowpane.getChildren().add(image);
             } else {
